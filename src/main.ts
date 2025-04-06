d86f6a79c315467bbcc7c14b99ab1eef0209e757
shajeebHaider <alihaidershajeeb06@gmail.com>
2025-04-06 15:53:08 +0600

    added interceptors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DataResponseInterceptor } from './common/interceptors/data-response/data-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -29,6 +30,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   //enable cors
   app.enableCors();
+  app.useGlobalInterceptors(new DataResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,6 +15,8 @@ import {
   ValidationPipe,
   UseGuards,
   SetMetadata,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
@@ -68,6 +70,7 @@ export class UsersController {
   @Post()
   //@SetMetadata('authType', 'None')
   @Auth(AuthType.None)
+  @UseInterceptors(ClassSerializerInterceptor)
   public createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
