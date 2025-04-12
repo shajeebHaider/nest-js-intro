6d4f08181ef66588d98fb0ff0b0035dfc82bb746
shajeebHaider <alihaidershajeeb06@gmail.com>
2025-04-12 11:33:28 +0600

    inline css in mail module allowed

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -25,7 +25,9 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
         },
         template: {
           dir: join(__dirname, 'templates'),
-          adapter: new EjsAdapter(),
+          adapter: new EjsAdapter({
+            inlineCssEnabled: true,
+          }),
           options: {
             strict: false,
           },
